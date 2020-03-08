4aa9987704099fa2477b9ff8cccb97cf398abf84
Alice BUI <anh.bui@knorex.com>
2020-03-08 18:24:10 +0700

    [WIP] -> Create post screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:facefood/models/user_details.dart';
 import 'package:facefood/restart_app.dart';
 import 'package:facefood/screens/guest/home.dart';
-<<<<<<< HEAD
-import 'package:facefood/screens/user/create_post.dart';
-=======
 import 'package:facefood/screens/register.dart';
->>>>>>> 68d20a4d85ee923ea0a684e99df6eecf1674a240
 import 'package:facefood/screens/user/home.dart';
 import 'package:facefood/utils/secure_storage.dart';
 import 'package:flutter/material.dart';
@@ -39,11 +35,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/user': (context) => UserHomeScreen(),
         '/guest': (context) => GuestHomeScreen(),
-<<<<<<< HEAD
-        '/create_post': (context) => CreatePostScreen(),
-=======
-         '/register':(context) => RegisterScreen(),
->>>>>>> 68d20a4d85ee923ea0a684e99df6eecf1674a240
+        //'/create_post': (context) => CreatePostScreen(),
       },
     );
   }
