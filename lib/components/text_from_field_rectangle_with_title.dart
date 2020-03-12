05c265ce2d442af592d52d3cffc06207fdc64625
LocTr <34329195+LocTr@users.noreply.github.com>
2020-03-12 16:35:37 +0700

    add TextFormFieldRectangleWithTitle component

---
+++ lib/components/text_from_field_rectangle_with_title.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:facefood/style/style.dart';
+
+class TextFormFieldRectangleWithTitle extends StatelessWidget {
+  final bool obscureText;
+  final String hintText, titleText;
+  final Function onSaved, validator;
+  TextFormFieldRectangleWithTitle(
+      {this.hintText,
+      this.obscureText = false,
+      this.onSaved,
+      this.validator,
+      this.titleText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width / 3.5,
+          child: Text(
+            titleText,
+            style: textStyleDefaultPrimary,
+          ),
+        ),
+        Expanded(
+                  child: TextFormField(
+            obscureText: obscureText,
+            style: textStyleDefault,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: hintText,
+              border: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(20.0),
+                ),
+                borderSide: BorderSide(
+                  style: BorderStyle.solid,
+                ),
+              ),
+            ),
+            validator: validator,
+            onSaved: onSaved,
+          ),
+        ),
+      ],
+    );
+  }
+}
