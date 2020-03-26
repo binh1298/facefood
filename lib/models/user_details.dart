e01a5bf6fdad48cdd531da5794d59111f248bc9b
Binh Pham <binh1298@gmail.com>
2020-03-27 06:22:04 +0700

    Remove unnecessary braces

--- lib/models/user_details.dart
+++ lib/models/user_details.dart
@@ -32,7 +32,7 @@ class UserDetails {
 Future<UserDetails> fetchUserDetails(String userId) async {
   final http.Response response = await apiCaller.get(
       route:
-          '${apiRoutes.createAdminRoute(apiRoutes.getUserProfile)}/${userId}');
+          '${apiRoutes.createAdminRoute(apiRoutes.getUserProfile)}/$userId');
   if (response.statusCode == 200) {
     var userDetailsJson = json.decode(response.body)['user'];
     return UserDetails.fromJson(userDetailsJson);
