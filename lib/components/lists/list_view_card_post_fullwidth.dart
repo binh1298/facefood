9ca88c2397c9f8ef6d60726fe06c5f643cb25dc5
Binh Pham <binh1298@gmail.com>
2020-03-27 05:52:26 +0700

    Add follow button to list user details card

--- lib/components/lists/list_view_card_post_fullwidth.dart
+++ lib/components/lists/list_view_card_post_fullwidth.dart
@@ -1,35 +1,43 @@
+import 'package:facefood/components/buttons/button_full_width.dart';
 import 'package:facefood/components/cards/card_post_fullwidth.dart';
 import 'package:facefood/components/cards/card_user_detail_info.dart';
 import 'package:facefood/models/post.dart';
 import 'package:facefood/models/user_profile_info.dart';
-import 'package:facefood/style/style.dart';
 import 'package:flutter/material.dart';
 
 class ListViewPost extends StatelessWidget {
   final List<Post> listPost;
   final UserProfileInfo userProfileInfoInfo;
   final bool isCurrentUser;
-  const ListViewPost({
-    Key key,
-    this.listPost,
-    this.userProfileInfoInfo,
-    this.isCurrentUser = true
-  }) : super(key: key);
+  const ListViewPost(
+      {Key key,
+      this.listPost,
+      this.userProfileInfoInfo,
+      this.isCurrentUser = true})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (listPost.isNotEmpty) {
-      return Flexible(
-        child: ListView(
-          padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+      return Padding(
+        padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+        child: Column(
           children: <Widget>[
-            isCurrentUser ? CardUserDetailInfo(
+            CardUserDetailInfo(
               avatarUrl: userProfileInfoInfo.avatarUrl,
               username: userProfileInfoInfo.username,
               followerCount: userProfileInfoInfo.followerCount,
               followingCount: userProfileInfoInfo.followingCount,
               postCount: userProfileInfoInfo.postCount,
-            ) : SizedBox(height: 0,),
+            ),
+            isCurrentUser
+                ? SizedBox(
+                    height: 0,
+                  )
+                : ButtonFullWidth(
+                    label: 'Follow',
+                    onPressed: () {},
+                  ),
             Divider(),
             SizedBox(
               height: 20,
@@ -49,13 +57,9 @@ class ListViewPost extends StatelessWidget {
                   )
                   .toList(),
             ),
-            // RaisedButton(
-            //   onPressed: () {},
-            //   textColor: colorTextPrimary,
-            //   color: colorPrimary,
-            //   child: Text('Load more'),
-            // ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
           ],
         ),
       );
