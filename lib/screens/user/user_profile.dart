d59fe1c4089ed3a88c21ba84842f791c2d105107
Binh Pham <binh1298@gmail.com>
2020-03-27 05:53:44 +0700

    Move appbar and user_profile_list_view into user profile

--- lib/screens/user/user_profile.dart
+++ lib/screens/user/user_profile.dart
@@ -1,5 +1,8 @@
+import 'package:facefood/components/cards/card_user_detail_info.dart';
 import 'package:facefood/models/user_profile_info.dart';
 import 'package:facefood/components/lists/list_view_card_post_fullwidth.dart';
+import 'package:facefood/style/style.dart';
+import 'package:facefood/utils/authorization.dart';
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatefulWidget {
@@ -17,55 +20,58 @@ class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: FutureBuilder<UserProfileInfo>(
-          future: fetchCurrentUserProfileInfo(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                  // ButtonFullWidth(
-                  //   label: 'Follow',
-                  //   onPressed: () {},
-                  // ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Divider(),
-                  ListViewPost(
-                    listPost: snapshot.data.totalPosts,
-                    userProfileInfoInfo: snapshot.data,
-                  ),
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              return Text('Unable to fetch this post');
-            } else
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-          },
+      appBar: AppBar(
+        backgroundColor: colorAppBar,
+        title: Text(
+          'Profile',
+          style: textStyleHeading,
         ),
-
-        // child: FutureBuilder<UserDetails>(
-        //   future: fetchUserProfile(),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.hasData) {
-        //       return Center(
-        //         child: Text(snapshot.data.username),
-        //       );
-        //     }
-        //     else if (snapshot.hasError) {
-        //       return Text(snapshot.error.toString());
-        //     }
-        //     else return LinearProgressIndicator();
-        //   },
-        // ),
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              logout(context);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<UserProfileInfo>(
+        future: fetchCurrentUserProfileInfo(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                CardUserDetailInfo(
+                  avatarUrl: snapshot.data.avatarUrl,
+                  username: snapshot.data.username,
+                  followerCount: snapshot.data.followerCount,
+                  followingCount: snapshot.data.followingCount,
+                  postCount: snapshot.data.postCount,
+                ),
+                Divider(),
+                ListViewPost(
+                  listPost: snapshot.data.totalPosts,
+                  userProfileInfoInfo: snapshot.data,
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return Text('Unable to fetch this post');
+          } else
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+        },
       ),
     );
   }
