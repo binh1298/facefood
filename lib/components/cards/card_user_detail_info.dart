d18f6b12848a6cd3fa5958ebce98d6b710d3d070
Binh Pham <binh1298@gmail.com>
2020-04-10 05:06:12 +0700

    Implement update avatar

--- lib/components/cards/card_user_detail_info.dart
+++ lib/components/cards/card_user_detail_info.dart
@@ -1,30 +1,45 @@
 import 'package:facefood/components/box_single_detail_info.dart';
+import 'package:facefood/models/user_profile_info.dart';
 import 'package:facefood/style/style.dart';
+import 'package:facefood/utils/snack_bar.dart';
 import 'package:flutter/material.dart';
 
 class CardUserDetailInfo extends StatelessWidget {
   final String username, avatarUrl;
   final int postCount, followerCount, followingCount;
-
-  const CardUserDetailInfo({
-    Key key,
-    this.username,
-    this.avatarUrl,
-    this.postCount,
-    this.followerCount,
-    this.followingCount,
-  }) : super(key: key);
+  final Function refreshList;
+  const CardUserDetailInfo(
+      {Key key,
+      this.username,
+      this.avatarUrl,
+      this.postCount,
+      this.followerCount,
+      this.followingCount,
+      this.refreshList})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print('avt: $avatarUrl');
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Row(
         children: <Widget>[
           // Avartar container
           GestureDetector(
-            onTap: () {
-              print('hihihi');
+            onTap: () async {
+              final imageUrl = await Navigator.pushNamed(
+                context,
+                '/updateImage',
+              );
+              print(imageUrl);
+              if (imageUrl != null) {
+                bool success = await putUserAvatar(username, imageUrl);
+                if (!success) {
+                  showErrorSnackBar(context, 'Update Avatar Failed');
+                }
+                refreshList();
+              }
             }, // implement on tap
             child: Container(
               // NO PADDING HERE
